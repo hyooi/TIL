ca880693e58b52a6b43589d6d4f0ff6a79b629df
HK <hyoin01@gmail.com>
2021-01-29 23:09:13 +0900

    feat. hystrix
    add circuit breaker pattern with hystrix

--- til.msa/display/src/main/java/com/elevenst/api/DisplayController.java
+++ til.msa/display/src/main/java/com/elevenst/api/DisplayController.java
@@ -1,5 +1,6 @@
 package com.elevenst.api;
 
+import com.elevenst.service.ProductRemoteService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/displays")
 public class DisplayController {
 
-    @GetMapping(path = "/{displayId}")
-    public String getDisplayDetail(@PathVariable String displayId) {
-        String productInfo = "[unknown]";
-        return String.format("[display id = %s at %s %s ]", displayId, System.currentTimeMillis(), productInfo);
-    }
+  private final ProductRemoteService productRemoteService;
+
+  public DisplayController(ProductRemoteService productRemoteService) {
+    this.productRemoteService = productRemoteService;
+  }
+
+  @GetMapping(path = "/{displayId}")
+  public String getDisplayDetail(@PathVariable String displayId) {
+    String productInfo = productRemoteService.getProductInfo(displayId);
+    return String
+        .format("[display id = %s at %s %s ]", displayId, System.currentTimeMillis(), productInfo);
+  }
 }
---
+++ til.msa/display/src/main/java/com/elevenst/service/ProductRemoteServiceImpl.java
@@ -0,0 +1,28 @@
+package com.elevenst.service;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class ProductRemoteServiceImpl implements ProductRemoteService {
+
+  public static final String URL = "http://localhost:8082/products/";
+
+  private final RestTemplate restTemplate;
+
+  public ProductRemoteServiceImpl(RestTemplate restTemplate) {
+    this.restTemplate = restTemplate;
+  }
+
+  @Override
+  @HystrixCommand(fallbackMethod = "getProductInfoFallback")
+  public String getProductInfo(String productId) {
+    return restTemplate.getForObject(URL + productId, String.class);
+  }
+
+  public String getProductInfoFallback(String productId, Throwable t) {
+    System.out.println("t = " + t);
+    return "[This product is sold out]";
+  }
+}
