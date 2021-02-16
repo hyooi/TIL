a1df62b62d9f6b3fe9f53906bd3a0f4c5bf2ece6
HK <hyoin01@gmail.com>
2021-02-16 22:53:33 +0900

    feat. AlternatingCharacters
    Interview Preparation Kit

---
+++ til.hackerrank/src/easy/AlternatingCharacters.java
@@ -0,0 +1,46 @@
+package easy;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class AlternatingCharacters {
+
+  // Complete the alternatingCharacters function below.
+  static int alternatingCharacters(String s) {
+    int deleteCnt = 0;
+    char before = 0;
+
+    for (int i = 0; i < s.length(); i++) {
+      char now = s.charAt(i);
+      if (before == now) {
+        deleteCnt++;
+      }
+      before = now;
+    }
+
+    return deleteCnt;
+  }
+
+  private static final Scanner scanner = new Scanner(System.in);
+
+  public static void main(String[] args) throws IOException {
+//    BufferedWriter bufferedWriter = new BufferedWriter(
+//        new FileWriter(System.getenv("OUTPUT_PATH")));
+
+    int q = scanner.nextInt();
+    scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+    for (int qItr = 0; qItr < q; qItr++) {
+      String s = scanner.nextLine();
+
+      int result = alternatingCharacters(s);
+      System.out.println(result);
+//      bufferedWriter.write(String.valueOf(result));
+//      bufferedWriter.newLine();
+    }
+
+//    bufferedWriter.close();
+
+    scanner.close();
+  }
+}
