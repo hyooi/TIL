b73b81832f88c0fbd6b99ecbf6cb8ff97a8c8ab2
HK <hyoin01@gmail.com>
2021-02-25 23:45:19 +0900

    feat. kotlin lecture3

--- til.kotlin/src/exercise/exercise3.kt
+++ til.kotlin/src/exercise/exercise3.kt
@@ -2,7 +2,7 @@ package exercise
 
 fun main(args: Array<String>) {
 
-    val sumFour: (Int, Int, Int, Int) -> Int = TODO()
+    val sumFour: (Int, Int, Int, Int) -> Int = { x1, x2, x3, x4 -> x1+x2+x3+x4 }
     val partialSumFour = sumFour.partialAppliedFunction(1, 2)
 
     require(partialSumFour(3, 4) == 10)
@@ -11,5 +11,5 @@ fun main(args: Array<String>) {
 
 fun <P1, P2, P3, P4, R> ((P1, P2, P3, P4) -> R).partialAppliedFunction(x: P1,
     y: P2): (P3, P4) -> R {
-    TODO()
-}
+        return { p3, p4 -> this(x, y, p3, p4) }
+    }
