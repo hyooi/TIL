041fa1d2cac78aefbddfe79a336d747249a9993b
HK <hyoin01@gmail.com>
2021-03-14 23:58:28 +0900

    feat.spring5
    - DispatcherServlet

---
+++ til.spring5/src/main/java/spring/dispatcherservlet/config/ApplicationInitializer2.java
@@ -0,0 +1,29 @@
+package spring.dispatcherservlet.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class ApplicationInitializer2 implements WebApplicationInitializer {
+
+  @Override
+  public void onStartup(ServletContext servletContext) throws ServletException {
+    AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+    context.register(AppConfig.class);
+
+    servletContext.addListener(new ContextLoaderListener(context));
+
+    AnnotationConfigWebApplicationContext dispatcherServlet =
+        new AnnotationConfigWebApplicationContext();
+    dispatcherServlet.register(DispatcherConfig.class);
+
+    ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher",
+        new DispatcherServlet(dispatcherServlet));
+    dispatcher.setLoadOnStartup(1);
+    dispatcher.addMapping("/");
+  }
+}
---
+++ til.spring5/src/main/java/spring/dispatcherservlet/config/ApplicationInitializer3.java
@@ -0,0 +1,41 @@
+package spring.dispatcherservlet.config;
+
+import javax.servlet.Filter;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
+
+public class ApplicationInitializer3 extends AbstractDispatcherServletInitializer {
+
+  @Override
+  protected WebApplicationContext createServletApplicationContext() {
+    return null;
+  }
+
+  @Override
+  protected String[] getServletMappings() {
+    return new String[]{"/"};
+  }
+
+  @Override
+  protected WebApplicationContext createRootApplicationContext() {
+    XmlWebApplicationContext context = new XmlWebApplicationContext();
+    context.setConfigLocations("/WEB-INF/dispatcher-servlet-context.xml");
+
+    return context;
+  }
+
+  @Override
+  protected Filter[] getServletFilters() {
+    return new Filter[] {new HiddenHttpMethodFilter(), new CharacterEncodingFilter() };
+  }
+}
