2ee8948a56f66eb583415f1021540ad98fd0aee2
HK <hyoin01@gmail.com>
2021-06-19 01:17:57 +0900

    feat. camel ftp

--- til.camel/src/main/java/ftp/FtpService.java
+++ til.camel/src/main/java/ftp/FtpService.java
@@ -3,28 +3,43 @@ package ftp;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.ProducerTemplate;
-import org.apache.camel.model.ModelCamelContext;
+import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.support.DefaultExchange;
 
 public class FtpService {
-  private final String username;
   private final String hostname;
+  private final String username;
+  private final String password;
   private final String directoryName;
 
   private final CamelContext context;
   private final ProducerTemplate template;
 
-  public FtpService(String username, String hostname, String directoryName,
+  public FtpService(String hostname, String username, String password,
+      String directoryName,
       CamelContext context, ProducerTemplate template) {
-    this.username = username;
     this.hostname = hostname;
+    this.username = username;
+    this.password = password;
     this.directoryName = directoryName;
     this.context = context;
     this.template = template;
   }
 
-  public Exchange run() {
-    var uri = "ftp:"+hostname+directoryName;
+  public Exchange run() throws Exception {
+    var uri = "ftp:"+username+"@"+hostname+directoryName+"?password="+password+"&allowNullBody"
+        + "=true&fileName=mydata-${date:now:yyyyMMdd}.txt";
+
+    context.addRoutes(new RouteBuilder() {
+      @Override
+      public void configure() throws Exception {
+        from(uri)
+            .log("ftp producing....")
+            .to("ftp:"+username+"@"+hostname+"/temp.txt?password="+password)
+            .log("ftp consuming...")
+            .end();
+      }
+    });
     return template.send(uri, new DefaultExchange(context));
   }
 }
--- til.camel/src/test/java/ftp/FtpServiceTest.java
+++ til.camel/src/test/java/ftp/FtpServiceTest.java
@@ -1,8 +1,13 @@
 package ftp;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 import org.apache.camel.test.junit5.CamelTestSupport;
+import org.apache.commons.net.ftp.FTPClient;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockftpserver.fake.FakeFtpServer;
@@ -14,6 +19,7 @@ import org.mockftpserver.fake.filesystem.UnixFakeFileSystem;
 class FtpServiceTest extends CamelTestSupport {
 
   private FakeFtpServer ftpServer;
+  private FTPClient ftpClient;
 
   @BeforeEach
   public void setUp() throws Exception {
@@ -22,23 +28,37 @@ class FtpServiceTest extends CamelTestSupport {
     fileSystem.add(new FileEntry("/home/foobar.txt", "abcdf 123456789"));
 
     ftpServer = new FakeFtpServer();
+    ftpServer.setServerControlPort(21);
     ftpServer.addUserAccount(new UserAccount("user", "password", "/home"));
     ftpServer.setFileSystem(fileSystem);
 
     ftpServer.start();
 
+    ftpClient = new FTPClient();
+    ftpClient.connect("localhost");
+    ftpClient.login("user", "password");
+
     super.setUp();
   }
 
   @Test
-  void test() {
+  void test() throws Exception {
+    assertEquals(1, ftpClient.listFiles("").length);
+
     var result =
-        new FtpService("user", "localhost", "/home/foobar.txt", context(), template()).run();
-    System.out.println(result);
+        new FtpService("localhost", "user", "password", "/", context(), template()).run();
+    assertEquals(226, result.getIn().getHeader("CamelFtpReplyCode", Integer.class));
+    assertEquals("mydata-"+ LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE) + ".txt",
+        result.getIn().getHeader("CamelFileNameProduced",
+        String.class));
+    assertEquals(2, ftpClient.listFiles("").length);
   }
 
   @Override
   public void tearDown() throws Exception {
+    super.tearDown();
+
+    ftpClient.disconnect();
     ftpServer.stop();
   }
 }
