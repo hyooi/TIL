f8e9810a5831bf513f50f1d0e39488debff97ba7
hyooi <hyoin01@gmail.com>
2021-06-21 08:53:12 +0900

    Create Ftp2FileService.java

---
+++ til.camel/src/test/java/ftp/Ftp2FileService.java
@@ -0,0 +1,18 @@
+package ftp;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.support.DefaultExchange;
+
+public class Ftp2FileService {
+  private final CamelContext context;
+  private final ProducerTemplate template;
+
+  public Ftp2FileService(CamelContext context, ProducerTemplate template) {
+    this.context = context;
+    this.template = template;
+  }
+
+  public Exchange run() 
