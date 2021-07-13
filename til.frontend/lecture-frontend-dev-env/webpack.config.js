e2d4e1474507984be86e411daa6a9e268ec15ed6
HK <hyoin01@gmail.com>
2021-07-14 00:51:07 +0900

    feat. plugin

--- til.frontend/lecture-frontend-dev-env/webpack.config.js
+++ til.frontend/lecture-frontend-dev-env/webpack.config.js
@@ -1,4 +1,8 @@
 const path = require("path")
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require("clean-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const webpack = require('webpack');
 
 module.exports = {
   mode: "development", //development
@@ -14,16 +18,17 @@ module.exports = {
       {
         test: /\.css$/, // .css 확장자로 끝나는 모든 파일
         use: [
-          "style-loader", // 개발 환경
+          process.env.NODE_ENV === "production"
+              ? MiniCssExtractPlugin.loader // 프로덕션 환경
+              : "style-loader", // 개발 환경
           "css-loader",
-        ], // 웹팩은 뒤에서부터 로더를 적용함.
+        ],
       },
       {
         test: /\.(png|jpg|gif|svg)$/, // .png 확장자로 마치는 모든 파일
         use: {
           loader: 'url-loader', // url 로더를 설정한다
           options: {
-            publicPath: './dist/', // file-loader와 동일
             name: '[name].[ext]?[hash]', // file-loader와 동일
             limit: 10000 // 10kb 미만 파일만 data url로 처리하고, 넘으면 file
             // loader로 처리
@@ -31,5 +36,26 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // 템플릿 경로를 지정
+      templateParameters: { // 템플릿에 주입할 파라매터 변수 지정
+        env: process.env.NODE_ENV === 'development' ? '(개발용)' : '',
+        minify: process.env.NODE_ENV === 'production' ? {
+          collapseWhitespace: true, // 빈칸 제거
+          removeComments: true, // 주석 제거
+        } : false,
+        hash: true,
+      },
+    }),
+    new CleanWebpackPlugin(),
+    new webpack.BannerPlugin({
+      banner: () => `빌드 날짜: ${new Date().toLocaleString()}`
+    }),
+    new CleanWebpackPlugin(),
+    ...(process.env.NODE_ENV === "production"
+        ? [new MiniCssExtractPlugin({filename: `[name].css`})]
+        : []),//production인 경우 css를 별도로 분리하여 속도 개선
+  ]
 }
