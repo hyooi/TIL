83c7ef8165d9eccf486114a4bcabffb8a930859e
HK <hyoin01@gmail.com>
2021-07-14 00:19:21 +0900

    feat. loader

--- til.frontend/lecture-frontend-dev-env/src/views/ResultView.js
+++ til.frontend/lecture-frontend-dev-env/src/views/ResultView.js
@@ -1,4 +1,5 @@
 import View from "./View.js";
+import defaultImage from "../images/default-image.jpg";
 
 export default class ResultView extends View {
   constructor(el) {
@@ -18,16 +19,16 @@ export default class ResultView extends View {
 
   getSearchResultsHtml(data) {
     return (
-      data.reduce((html, item) => {
-        html += this.getSearchItemHtml(item);
-        return html;
-      }, "<ul>") + "</ul>"
+        data.reduce((html, item) => {
+          html += this.getSearchItemHtml(item);
+          return html;
+        }, "<ul>") + "</ul>"
     );
   }
 
   getSearchItemHtml(item) {
     return `<li>
-      <img src="${item.image}" onerror="this.src='src/images/default-image.jpg'"/>
+      <img src="${item.image}" onerror="this.src='${defaultImage}'"/>
       <p>${item.name}</p>
     </li>`;
   }
--- til.frontend/lecture-frontend-dev-env/webpack.config.js
+++ til.frontend/lecture-frontend-dev-env/webpack.config.js
@@ -9,4 +9,27 @@ module.exports = {
     filename: "[name].js", //name: entrypoint의 값
     path: path.resolve("./dist"), //resolve: 절대경로 가져옴
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // .css 확장자로 끝나는 모든 파일
+        use: [
+          "style-loader", // 개발 환경
+          "css-loader",
+        ], // 웹팩은 뒤에서부터 로더를 적용함.
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/, // .png 확장자로 마치는 모든 파일
+        use: {
+          loader: 'url-loader', // url 로더를 설정한다
+          options: {
+            publicPath: './dist/', // file-loader와 동일
+            name: '[name].[ext]?[hash]', // file-loader와 동일
+            limit: 10000 // 10kb 미만 파일만 data url로 처리하고, 넘으면 file
+            // loader로 처리
+          }
+        }
+      }
+    ]
+  }
 }
