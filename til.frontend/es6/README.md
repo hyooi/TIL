381ec7e00ee1e3e9ebcf0c4d10d56aca975cc8ac
hyoin <hyoin@gmail.com>
2021-07-16 13:06:03 +0900

    feat. init es6

---
+++ til.frontend/es6/README.md
@@ -0,0 +1,4 @@
+# ES6(=ES2015)
+- 개선된 javascript문법
+- browser 호환성 좋음
+- https://kangax.github.io/compat-table/es6/
---
+++ til.frontend/es6/let.js
@@ -0,0 +1,11 @@
+var name = "global  var";
+
+function home() {
+    var homevar = "homevar";
+    for(var i=0; i<100; i++) {
+
+    }
+    console.log(i);
+}
+
+home();
