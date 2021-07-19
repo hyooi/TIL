26cc94e09ad093e52222a4667289104513eb1432
HK <hyoin01@gmail.com>
2021-07-19 18:44:16 +0900

    feat. separate prod, dev

---
+++ til.frontend/boiler-plate/config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://testdb:test@hk.ypq3h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
--- til.frontend/boiler-plate/index.js
+++ til.frontend/boiler-plate/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 5000 //백엔드 서버의 포트!
 
+const config = require('./config/key')
 const bodyParser = require('body-parser');
 const { User } = require('./model/User');
 
@@ -9,7 +10,7 @@ app.use(bodyParser.urlencoded({extended: true})); // application/x-www-form-urle
 app.use(bodyParser.json()); //application/json
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://testdb:test@hk.ypq3h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log("Mongo db connected..."))
 .catch(err => console.log(err));
@@ -22,7 +23,6 @@ app.get('/', (req, res) => {
 app.post('/register', (req, res) => {
   const user = new User(req.body)
   user.save((err, userInfo) => {
-    console.log("result: ", err)
     if(err) return res.json({ success: false, err })
     return res.status(200).json({
       success: true
