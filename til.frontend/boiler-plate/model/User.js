8bb139bebf50b5bac960531fd79ce672806c7d29
HK <hyoin01@gmail.com>
2021-07-19 19:07:03 +0900

    feat. apply bcrypt

--- til.frontend/boiler-plate/model/User.js
+++ til.frontend/boiler-plate/model/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10
 
 const userSchema = mongoose.Schema({
   name: {
@@ -28,6 +30,27 @@ const userSchema = mongoose.Schema({
   },
   tokenExpiration: {
     type: Number
+  }
+})
+
+userSchema.pre('save', function (next) {
+  var user = this;
+
+  if(user.isModified('password')) {
+    //패스워드 암호화
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if(err) {
+        return next(err)
+      }
+
+      bcrypt.hash(user.password, salt,function (err, hash) {
+        if(err) return next(err)
+        user.password = hash
+
+        next()
+      })
+    })
+
   }
 })
 
