c5f1927095184ebabe42930feeb6329e8f2b7fd7
HK <hyoin01@gmail.com>
2021-07-21 14:15:45 +0900

    feat. redux

--- til.frontend/boiler-plate/client/src/_reducers/index.js
+++ til.frontend/boiler-plate/client/src/_reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux'; //reducer가 여러개인 경우 combineReducer로 합쳐줌
-// import user from './user-reducer';
+import user from './user_reducer';
 
 const rootReducer = combineReducers({
-  //user
+  user
 })
 
 export default rootReducer
--- til.frontend/boiler-plate/client/src/components/views/LoginPage/LoginPage.js
+++ til.frontend/boiler-plate/client/src/components/views/LoginPage/LoginPage.js
@@ -1,9 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
+import {useDispatch} from "react-redux";
+import {loginUser} from "../../../_actions/user_action";
 
 function LoginPage() {
+  const dispatch = useDispatch()
+  const [Email, setEmail] = useState("")
+  const [Password, setPassword] = useState("")
+
+  const onEmailHandler = (event) => {
+    setEmail(event.currentTarget.value)
+  }
+
+  const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value)
+  }
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault(); //화면 리프레시 막아줌
+    dispatch(loginUser({
+      email: Email,
+      password: Password
+    })) //redux를 위해 component에서 action함
+  }
+
   return (
-    <div>
-      LoginPage
+    <div style={{
+      display: 'flex', justifyContent: 'center', alignItems: 'center',
+      width: '100%', height: '100vh'
+    }}>
+      <form style={{ display: 'flex', flexDirection: 'column'}}
+        onSubmit={onSubmitHandler}>
+        <label>Email</label>
+        <input type="email" value={Email} onChange={onEmailHandler} />
+        <label>Password</label>
+        <input type="password" value={Password} onChange={onPasswordHandler} />
+        <br/>
+        <button type="submit">Login</button>
+      </form>
     </div>
   )
 }
