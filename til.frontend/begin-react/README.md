58ded2c5495a2c000b78656e373b014d7f09cc59
HK <hyoin01@gmail.com>
2021-08-10 01:47:03 +0900

    feat. css

--- til.frontend/begin-react/README.md
+++ til.frontend/begin-react/README.md
@@ -2,3 +2,17 @@
 - 브라우저에 실제로 보여지는 dom이 아니라, 메모리에 가상으로 존재하는 virtual dom을 활용
 - 따라서 virtual dom을 사용하면 실제로 브라우저에서 dom을 사용하는 것보다 훨씬 빠름
 - react는 virtual dom이 변경되면, 실제 브라우저의 dom과 비교해 변경된 부분을 실제 dom에 패치함
+
+
+## JSX
+- 리액트 컴포넌트에서 xml 형식의 값을 반환
+
+### 스타일 지정
+- 인라인으로 지정 시: 객체 형태로 작성해야 하며, 카멜케이스를 사용해야 함
+```html
+  <div style={style}>{name}</div>
+```
+- css사용 시: class가 아닌 className으로 지정
+```html
+  <div className="gray-box"></div>
+```
--- til.frontend/begin-react/src/App.js
+++ til.frontend/begin-react/src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Hello from "./Hello";
 
 function App() {
+  const name = 'react';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 24,
+    padding: '1rem'
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*주석!*/}
+      <Hello
+        //여기선 이렇게 주석쓰기 가능
+      />
+      <div style={style}>{name}</div>
+      <div className="gray-box"/>
     </div>
   );
 }
