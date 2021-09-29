7ff58c91d95ff7671998c18f167ec800f23c5bef
HK <hyoin01@gmail.com>
2021-09-30 00:04:06 +0900

    feat. codingtest

--- til.hackerrank/src/main/java/etc/codility/CodingTest4.java
+++ til.hackerrank/src/main/java/etc/codility/CodingTest4.java
@@ -10,6 +10,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Queue;
 import java.util.Random;
+import java.util.RandomAccess;
 import java.util.Set;
 import java.util.Stack;
 
@@ -17,12 +18,24 @@ public class CodingTest4 {
 
 
   public static String solution(int N) {
-    Stack<Character> chars = new Stack<>();
-    chars.addAll(asList('a', 'b', 'c'));
+    List<Character> chars = new ArrayList<>(asList('a', 'b', 'c', 'd','e', 'f', 'g', 'h','i','j',
+        'k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'));
+
+    Random random = new Random();
+    int randomIdx = random.nextInt(chars.size());
+    char randomChar = chars.get(randomIdx);
+    chars.remove(randomIdx);
 
     StringBuilder result = new StringBuilder();
-    for (int i=0; i < N%3; i++) {
-      result.append(chars.pop());
+    if(N%2 == 0) {
+      for(int i=0; i<N-1; i++) {
+        result.append(randomChar);
+      }
+      result.append(chars.get(random.nextInt(chars.size())));
+    } else {
+      for(int i=0; i<N; i++) {
+        result.append(randomChar);
+      }
     }
 
     return result.toString();
